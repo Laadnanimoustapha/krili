51abdc5e3cae6ece510a973065cdedc52d616ffc
mustapha laadnani <mustapha.laadnani.03@edu.uiz.ac.ma>
2025-11-23 18:21:09 +0100

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -4,4 +4,12 @@ if (!process.env.DATABASE_URL) {
     throw new Error('DATABASE_URL environment variable is not defined');
 }
 
-export const db = mysql.createPool(process.env.DATABASE_URL);
+export const db = mysql.createPool({
+    uri: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+});
