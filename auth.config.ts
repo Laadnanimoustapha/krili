cc4426aa61b0317eb0ec743d7ba3921fc8d3a466
mustapha laadnani <mustapha.laadnani.03@edu.uiz.ac.ma>
2025-11-24 22:46:53 +0100

    login error 📛🛑

--- auth.config.ts
+++ auth.config.ts
@@ -1,4 +1,27 @@
 import type { NextAuthConfig } from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+import { z } from 'zod';
+import { db } from '@/lib/db';
+import bcrypt from 'bcryptjs';
+import type { RowDataPacket } from 'mysql2';
+
+interface User extends RowDataPacket {
+    id: number;
+    email: string;
+    password: string;
+    first_name: string;
+    last_name: string;
+}
+
+async function getUser(email: string): Promise<User | undefined> {
+    try {
+        const [rows] = await db.execute<User[]>('SELECT * FROM users WHERE email = ?', [email]);
+        return rows[0];
+    } catch (error) {
+        console.error('Failed to fetch user:', error);
+        throw new Error('Failed to fetch user.');
+    }
+}
 
 export const authConfig = {
     pages: {
@@ -17,5 +40,25 @@ export const authConfig = {
             return true;
         },
     },
-    providers: [], // Add providers with an empty array for now
+    providers: [
+        Credentials({
+            async authorize(credentials) {
+                const parsedCredentials = z
+                    .object({ email: z.string().email(), password: z.string().min(6) })
+                    .safeParse(credentials);
+
+                if (parsedCredentials.success) {
+                    const { email, password } = parsedCredentials.data;
+                    const user = await getUser(email);
+                    if (!user) return null;
+                    const passwordsMatch = await bcrypt.compare(password, user.password);
+
+                    if (passwordsMatch) return user;
+                }
+
+                console.log('Invalid credentials');
+                return null;
+            },
+        }),
+    ],
 } satisfies NextAuthConfig;
