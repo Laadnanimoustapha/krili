ed56551997d8d902d2acff16e0636219b08552e2
mustapha laadnani <mustapha.laadnani.03@edu.uiz.ac.ma>
2025-11-26 21:00:50 +0100

    Update user-menu.tsx

--- components/user-menu.tsx
+++ components/user-menu.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { signOut } from "next-auth/react"
+import { signOutAction } from "@/app/actions/auth"
 import Link from "next/link"
 import {
   DropdownMenu,
@@ -63,13 +63,14 @@ export function UserMenu({ user, language, getTranslation }: UserMenuProps) {
           </Link>
         </DropdownMenuItem>
         <DropdownMenuSeparator />
-        <DropdownMenuItem
-          className="cursor-pointer text-red-600 focus:text-red-600 focus:bg-red-50"
-          onClick={() => signOut({ callbackUrl: "/" })}
-        >
-          <LogOut className="mr-2 h-4 w-4" />
-          <span>Logout</span>
-        </DropdownMenuItem>
+        <form action={signOutAction} className="w-full">
+          <DropdownMenuItem asChild>
+            <button type="submit" className="w-full flex items-center cursor-pointer text-red-600 focus:text-red-600 focus:bg-red-50">
+              <LogOut className="mr-2 h-4 w-4" />
+              <span>Logout</span>
+            </button>
+          </DropdownMenuItem>
+        </form>
       </DropdownMenuContent>
     </DropdownMenu>
   )
