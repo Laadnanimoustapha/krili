7e78ff78506adf05d79db88ca89fc949acbc520a
mustapha laadnani <mustapha.laadnani.03@edu.uiz.ac.ma>
2025-11-28 20:23:02 +0100

    wayiiiiiiiiiiiiiiih

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,9 @@
 import { auth } from "@/auth"
 import { NavbarClient } from "@/components/navbar-client"
+import { getDictionary } from "@/dictionaries/get-dictionary"
 
-export async function Header() {
+export async function Header({ locale }: { locale: string }) {
   const session = await auth()
-  return <NavbarClient user={session?.user} />
+  const dictionary = await getDictionary(locale)
+  return <NavbarClient user={session?.user} dictionary={dictionary} />
 }
