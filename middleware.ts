248083a645ef690dc82d99964bb285fda65af571
mustapha laadnani <mustapha.laadnani.03@edu.uiz.ac.ma>
2025-11-29 14:14:49 +0100

    Create middleware.ts

---
+++ middleware.ts
@@ -0,0 +1,24 @@
+import { auth } from "@/auth"
+import { NextResponse } from "next/server"
+
+export default auth((req) => {
+    if (!req.auth) {
+        return NextResponse.redirect(new URL("/login", req.url))
+    }
+})
+
+export const config = {
+    matcher: [
+        "/profile/:path*",
+        "/billing/:path*",
+        "/notifications/:path*",
+        "/messages/:path*",
+        "/my-items/:path*",
+        "/my-listings/:path*",
+        "/list-item/:path*",
+        "/list-items/:path*",
+        "/wishlist/:path*",
+        "/browse/:path*",
+        "/search/:path*",
+    ],
+}
